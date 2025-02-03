6d888a12254ee3922e4e692f7c0d20d74a497941
Hansika <22wh1a0430@bvrithyderabad.edu.in>
2025-02-03 21:58:54 +0530

    firstcommit

---
+++ RandomNumberGenerator.java
@@ -0,0 +1,12 @@
+import java.util.Random;
+
+public class RandomNumberGenerator {
+    public static void main(String[] args) {
+        Random random = new Random();
+        
+        // Generate a random integer between 1 and 100
+        int randomNumber = random.nextInt(100) + 1;
+        
+        System.out.println("Generated Random Number: " + randomNumber);
+    }
+}
